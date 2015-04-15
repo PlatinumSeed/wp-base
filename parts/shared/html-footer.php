7db527056dbed17c8a34e688335dae7e8e2a83f9
Riaan Laubscher <riaan@platinumseed.com>
2015-04-15 16:24:09 +0200

    Moved vendor.js location and updated favicons

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,6 +1,5 @@
         </div>
         <?php wp_footer(); ?>
-        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor.min.js"></script>
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/app.js"></script>
 	</body>
 </html>
--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -14,18 +14,30 @@
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
         <!-- Favicons -->
+		<!--  http://www.favicon-generator.org/ -->
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
-        <!-- For third-generation iPad with high-resolution Retina display: -->
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicon-144x144.png">
-        <!-- For iPhone with high-resolution Retina display: -->
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicon-114x114.png">
-        <!-- For first- and second-generation iPad: -->
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicon-72x72.png">
-        <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
-        <link rel="apple-touch-icon-precomposed" href="favicon-72x72.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_stylesheet_directory_uri(); ?>/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-16x16.png">
+		<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
 
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/vendor.min.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/screen.css" type="text/css" />
+
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor.min.js"></script>
 
         <?php wp_head(); ?>
     </head>
